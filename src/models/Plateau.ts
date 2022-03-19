6733309661e1e414ef0f32c9e151df3890d38c97
fdttests <filipedtristao3@gmail.com>
2022-03-19 17:12:30 -0300

    feat: add models

---
+++ src/models/Plateau.ts
@@ -0,0 +1,21 @@
+export default class Plateau {
+    public minY: number = 0;
+    public minX: number = 0;
+    
+    public constructor(
+        public maxY: number = 0,
+        public maxX: number = 0
+    ) {
+        this.validate();
+    }
+
+    private validate() {
+        if (this.maxY <= this.minY) {
+            throw new Error('Invalid plateau size!');
+        }
+
+        if (this.maxX <= this.minX) {
+            throw new Error('Invalid plateau size!');
+        }
+    }
+}
