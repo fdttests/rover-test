b17a85b1f6bb91c8e0a2b7752d8849bbee4cf829
fdttests <filipedtristao3@gmail.com>
2022-03-19 22:14:57 -0300

    feat: add parse instructions test

--- tests/ParseFileIntoRoverInstructions.test.ts
+++ tests/ParseFileIntoRoverInstructions.test.ts
@@ -1,23 +1,21 @@
 import { DirectionEnum } from "../src/enums/DirectionEnum";
 import ParseFileIntoRoverInstructionsUseCase from "../src/use-cases/ParseFileIntoRoverInstructionsUseCase";
+import * as fs from 'fs';
 
 describe('Parse File Into Rover Instructions Test', () => {
     it('should parse the instructions given the file content', () => {
         const parseFileIntoRoverInstructionsUseCase = new ParseFileIntoRoverInstructionsUseCase();
-        const mockContent = `
-        5 5
-        1 2 N
-        LMLMLMLMM
-        3 3 E
-        MMRMMRMRRM`;
+        const fileContent = fs.readFileSync('./tests/mocks/normalized-instructions.txt', 'utf8');
 
-        const instructions = parseFileIntoRoverInstructionsUseCase.execute(mockContent);
+        const instructions = parseFileIntoRoverInstructionsUseCase.execute(fileContent);
         const [instructionOne, instructionTwo] = instructions.instructions;
 
         expect(instructions.plateau.minX).toBe(0);
         expect(instructions.plateau.maxX).toBe(5);
         expect(instructions.plateau.minY).toBe(0);
         expect(instructions.plateau.maxY).toBe(5);
+
+        expect(instructions.instructions.length).toBe(2);
 
         expect(instructionOne.rover.xPosition).toBe(1);
         expect(instructionOne.rover.yPosition).toBe(2);
@@ -26,7 +24,7 @@ describe('Parse File Into Rover Instructions Test', () => {
 
         expect(instructionTwo.rover.xPosition).toBe(3);
         expect(instructionTwo.rover.yPosition).toBe(3);
-        expect(instructionOne.rover.direction).toBe(DirectionEnum.East);
-        expect(instructionOne.instruction).toBe('MMRMMRMRRM');
+        expect(instructionTwo.rover.direction).toBe(DirectionEnum.East);
+        expect(instructionTwo.instruction).toBe('MMRMMRMRRM');
     });
 });
